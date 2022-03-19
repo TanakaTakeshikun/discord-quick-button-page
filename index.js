743b8c7b5a93977674d8f7cc06026a26ef27d227
BURI <85824872+tanakataku@users.noreply.github.com>
2022-03-19 20:21:21 +0900

    コードの短縮(メモリー使用量削減)

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 //createbyBURI
-let buttonname=[],
-error,
-content = [],
-loop = [],
-customid=[];
+let error,
+ data=[];
 const buttonfunc = (nextid,backid,obj,name,json) => {
   return buttonobj({nextid:`${obj.next}${nextid}`,nextlabel:name.next,backid:`${obj.back}${backid}`,backlabel:name.back,json:json})
 };
@@ -28,14 +25,12 @@ const buttonfunc = (nextid,backid,obj,name,json) => {
      let data = { components: [
         {
           custom_id:button.backid,
-          disabled: false,
           label: button.backlabel,
           style: 1,
           type: 2,
         },
         {
           custom_id: button.nextid,
-          disabled: false,
           label: button.nextlabel,
           style: 1,
           type: 2,
@@ -53,26 +48,23 @@ module.exports = {
     buttonerror: e => error = e,
     buttonpage: a =>{
       if(!a.id) throw new Error('dont know id');
-    content[a.id] = a.content||["notcontent"];
-    loop[a.id] = a.loop||false,
-    customid[a.id]=a.customid,
-    buttonname[a.id]=a.name;
-    const buttondata = buttonfunc(1,(loop[a.id])?content[a.id].length-1:0,{back:a.customid?.back||"BURIback",next:a.customid?.next||"BURInext"},{next:a.name?.next||"next",back:a.name?.back||"back"},a.json)
-      return {content:content[a.id][0],data:buttondata,page:1};
+    data[a.id]={buttonname:a.name||{next:"next",back:"back"},customid:a.customid||{next:"BURInext",back:"BURIback"},loop:a.loop||false,content:a.content||["not cotent"]}
+    const buttondata = buttonfunc(1,(data[a.id].loop)?data[a.id].content.length-1:0,{back:data[a.id].customid.back,next:data[a.id].customid.next},{next:data[a.id].name.next,back:data[a.id].name.back},a.json)
+      return {content:data[a.id].content[0],data:buttondata,page:1};
     },
     buttonpush:obj=>{
       if (!obj.interaction.isButton())return 0;
       if(!obj.id) throw new Error('dont know id');
- if(!content[obj.id])return{content:error?.content||"notcontent",data:errorbutton(),page:0};
+      if(!data[obj.id])return{content:error?.content||"not content",data:errorbutton(),page:0};
       const id = obj.interaction.customId.replace(/[^0-9]/g, ''),
        number = Number(id);
-      if(obj.interaction.customId.startsWith(customid[obj.id]?.next||"BURInext")){
-       const buttondata = buttonfunc((number+1>=content[obj.id].length)?(loop[obj.id])?0:number:number+1,number-1,{back:customid[obj.id]?.back||"BURIback",next:customid[obj.id]?.next||"BURInext"},{next:buttonname[obj.id]?.next||"next",back:buttonname[obj.id]?.back||"back"},obj.json);
-       return {content:content[obj.id][number],data:buttondata,page:number+1};
+      if(obj.interaction.customId.startsWith(data[obj.id].customid.next)){
+       const buttondata = buttonfunc((number+1>=data[obj.id].content.length)?(data[obj.id].loop)?0:number:number+1,number-1,{back:data[obj.id].customid.back,next:data[obj.id].customid.next},{next:data[obj.id].buttonname.next,back:data[obj.id].buttonname.back},obj.json);
+       return {content:data[obj.id].content[number],data:buttondata,page:number+1};
       }
-      if(obj.interaction.customId.startsWith(customid[obj.id]?.back||"BURIback")){
-        const buttondata = buttonfunc((content[obj.id].length-1==number)?0:number+1,(number-1<0)?(loop[obj.id])?content[obj.id].length-1:0:number-1,{back:customid[obj.id]?.back||"BURIback",next:customid[obj.id]?.next||"BURInext"},{next:buttonname[obj.id]?.next||"next",back:buttonname[obj.id]?.back||"back"},obj.json);
-        return {content:content[obj.id][number],data:buttondata,page:number+1};
+      if(obj.interaction.customId.startsWith(data[obj.id].customid.back)){
+        const buttondata = buttonfunc((data[obj.id].content.length-1==number)?0:number+1,(number-1<0)?(data[obj.id].loop)?data[obj.id].content.length-1:0:number-1,{back:data[obj.id].customid.back,next:data[obj.id].customid.next},{next:data[obj.id].buttonname.next,back:data[obj.id].buttonname.back},obj.json);
+        return {content:data[obj.id].content[number],data:buttondata,page:number+1};
        }
     }
   };
